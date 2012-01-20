42b4ff00783800082b18f11b5d44971dc08380cb
German Krauss <german@devartis.com>
2012-01-20 10:37:38 -0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup (
+    name = "pyslideshare",
+    description = "Python module for SlideShare",
+    long_description = """
+Python module for using API offered by SlideShare
+""",
+    version = "0.2",
+    author = 'Marc Poulhiès',
+    author_email = 'dkm@kataplop.net',
+    url = "http://code.google.com/p/pyslideshare/",
+    maintainer = 'Marc Poulhiès',
+    maintainer_email = 'dkm@kataplop.net',
+    license = "GPL,MIT",
+    packages = ['pyslideshare'],
+    )
+
